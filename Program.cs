05b48c608854da1d625e8cfd1897631f2141a382
SmithTom <943866961@qq.com>
2024-10-24 09:07:45 +0800

    1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using NetCoreScvhost.Contract;
 using NetCoreSvchost.InternalImpl;
 using NetCoreSvchost.Options;
 using System.Text.Json.Serialization;
+
+var classId = Guid.Parse(ClassIds.TestV1);
+var interfaceId = Guid.Parse(ITest.IID);
+
+ClientImport.DllGetClassObject(ref classId, ref interfaceId, out var ppv);
+ITest test = ppv as ITest;
+var XS = test.Method(1,"ÕÅÈý");
+
+List<string> expected = ["1", "2±³¾°ÏÂ", "88888", "4", "5"];
+test.Show("152", expected.ToArray(), expected.Count);
 
 var builder = Host.CreateApplicationBuilder(args);
 
